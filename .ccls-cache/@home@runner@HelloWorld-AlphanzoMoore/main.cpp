d3e8ee15042ed93a197047d5cbca6bffe94c7b07
AlphanzoMoore <25640152-AlphanzoMoore@users.noreply.replit.com>
2023-09-26 14:53:38 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@HelloWorld-AlphanzoMoore/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World!\n";
+  std::cout << "I am Alphanzo Moore ";
+  std::cout << " Classes starts today on August 21 2023 -";
+  
+  std::cout << " And I am happy to learn CIS-5 Online With Mr Brian Kenney"; 
+  }
